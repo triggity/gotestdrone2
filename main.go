d933d3809713e85cb523fa900873c44216556cb1
Michael Truong <michael@cloudflare.com>
2016-03-04 16:21:01 -0800

    trying to trigger

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
+	input := []string{"foo", "bar", "thanks"}
 	fmt.Printf("Hello World")
+	fmt.Printf(strings.Join(input, ", "))
 
 }
