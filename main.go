cb8f8c0704876184abf782277e8865caaa85c323
Michael Truong <michael@cloudflare.com>
2016-03-05 15:12:57 -0800

    adding dots

--- main.go
+++ main.go
@@ -8,6 +8,6 @@ import (
 func main() {
 	input := []string{"foo", "bar", "thanks", "you", "fuck"}
 	fmt.Printf("Hello World")
-	fmt.Printf(strings.Join(input, ", "))
+	fmt.Printf(strings.Join(input, "..."))
 
 }
