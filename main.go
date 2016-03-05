6ffa589e579739dae9d38adadae632190352c650
Michael Truong <michael@cloudflare.com>
2016-03-05 14:08:44 -0800

    more

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	input := []string{"foo", "bar", "thanks"}
+	input := []string{"foo", "bar", "thanks", "you"}
 	fmt.Printf("Hello World")
 	fmt.Printf(strings.Join(input, ", "))
 
